6188d60b4396a5afb4ede8b7edd38971841a6a4c
Veselov Nikolay <veselovnd@gmail.com>
2023-05-10 17:43:29 +0300

    JwtAuthenticationEntryPoint and dto

--- src/main/java/ru/veselov/instazoocource/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/ru/veselov/instazoocource/security/JwtAuthenticationEntryPoint.java
@@ -1,22 +1,30 @@
 package ru.veselov.instazoocource.security;
 
-import jakarta.servlet.ServletException;
+import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import ru.veselov.instazoocource.payload.response.InvalidLoginResponse;
 
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-
 
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
-                         AuthenticationException authException) throws IOException, ServletException {
-
+                         AuthenticationException authException) throws IOException {
+        InvalidLoginResponse loginResponse = new InvalidLoginResponse();
+        String jsonLoginResponse = new Gson().toJson(loginResponse);
+        response.setContentType(SecurityConstants.CONTENT_TYPE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().println(jsonLoginResponse);
+        log.error("Invalid authentication data");
     }
 }
